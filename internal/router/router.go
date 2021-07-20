3c22fa2557a86f721de4d010662aa95639a9fbd3
shenwei <shenwei@89trillion.com>
2021-07-20 15:02:22 +0800

    增加异常处理

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,27 +10,27 @@ func InitRouter() *gin.Engine {
 
 	group1 := r.Group("")
 	{
-		group1.GET("/FindSoldierByRaUnCv", ctrl.FindSoldierByCvApi)
+		group1.GET("/FindSoldierByRaUnCv", ctrl.ReturnData(ctrl.FindSoldierByCvApi))
 	}
 
 	group2 := r.Group("")
 	{
-		group2.GET("/FindSoldierRaById", ctrl.FindSoldierRaByIdApi)
+		group2.GET("/FindSoldierRaById", ctrl.ReturnData(ctrl.FindSoldierRaByIdApi))
 	}
 
 	group3 := r.Group("")
 	{
-		group3.GET("/FindSoldierCoById", ctrl.FindSoldierCoByIdApi)
+		group3.GET("/FindSoldierCoById", ctrl.ReturnData(ctrl.FindSoldierCoByIdApi))
 	}
 
 	group4 := r.Group("")
 	{
-		group4.GET("/FindSoldierByCv", ctrl.FindSoldierByCvApi)
+		group4.GET("/FindSoldierByCv", ctrl.ReturnData(ctrl.FindSoldierByCvApi))
 	}
 
 	group5 := r.Group("")
 	{
-		group5.GET("/FindSoldierByUn", ctrl.FindSoldierByUnApi)
+		group5.GET("/FindSoldierByUn", ctrl.ReturnData(ctrl.FindSoldierByUnApi))
 	}
 
 	return r
